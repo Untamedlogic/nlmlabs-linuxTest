72e4eea796dc9b27c3b16469fbaed55a22537965
Untamedlogic <nathanmccoy1990@gmail.com>
2022-01-12 16:52:38 -0500

    Pushing all of the necessary files to create my Linux test box in AWS. Part two - Added Modules folder and a variable file

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,3 @@
-resource "aws_instance" "myec2" {
-   name = "Linux Test Machine"
-   ami = "ami-0d718c3d715cec4a7"
-   instance_type = var.instance_type
- }
+module "ec2module" {
+  source = "modules/ec2"
+}
