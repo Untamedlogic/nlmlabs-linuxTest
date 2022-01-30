e66a8a12ef05198e666baa70287540fe91f05c43
Untamedlogic <92870954+Untamedlogic@users.noreply.github.com>
2022-01-29 23:26:56 -0500

    Update ec2.tf
    Updating test Linux Deployment

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,16 @@
-module "ec2module" {
-  source = "nlmlabs-linuxTest/modules"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "Linux Test Machine"
+
+  ami                    = "ami-0d718c3d715cec4a7"
+  instance_type          =  var.instance_type
+  vpc_security_group_ids = ["sg-068b2f61f90b4d644"]
+  subnet_id              = "subnet-afd13bd2"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
